ee6a3a35062e985088cc0bf8429e23158ea2ff77
Guiled <guislain.duthieuw@gmail.com>
2014-03-23 15:38:05 +0100

    Adding a specific class for testing with lua

---
+++ tests/Luatoum.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace tests\unit;
+
+use atoum;
+
+class Luatoum extends atoum {
+
+    /**
+     *
+     * @var \Hoa\Compiler\Llk
+     */
+    private $compiler;
+
+    /**
+     *
+     * @var \Hoathis\Lua\Visitor\Interpreter
+     */
+    private $visitor;
+
+    protected function execute($input) {
+        $ast = $this->getCompiler()->parse($input);
+
+        return $this->getVisitor()->visit($ast);
+    }
+
+    protected function exec($input) {
+        $this->execute($input);
+        return $this;
+    }
+
+    protected function luaOutput($input) {
+        return parent::output(function () use($input) {
+            $this->exec($input);
+        });
+    }
+
+    public function getVisitor() {
+        if (!$this->visitor) {
+            $this->visitor = new \Hoathis\Lua\Visitor\Interpreter();
+        }
+
+        return $this->visitor;
+    }
+
+    public function getCompiler() {
+        if (!$this->compiler) {
+            $this->compiler = \Hoa\Compiler\Llk::load(
+                    new \Hoa\File\Read('Grammar.pp')
+            );
+        }
+
+        return $this->compiler;
+    }
+
+}
