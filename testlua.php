ee3a72686f6b1b980209e85752a412fe0f5ca9ec
Guiled <guislain.duthieuw@gmail.com>
2014-03-26 23:26:32 +0100

    Create a php file to shortly compile and execute a lua string

--- testlua.php
+++ testlua.php
@@ -1,29 +1,28 @@
 <?php
+
 require '../../Hoa/Core/Core.php';
 
 from('Hoa')
--> import('File.Read')
--> import('Compiler.Llk.~');
+    ->import('File.Read')
+    ->import('Compiler.Llk.~');
 
 from('Hoathis')
--> import('Lua.Visitor.Interpreter');
+    ->import('Lua.Visitor.Interpreter');
 $compiler = \Hoa\Compiler\Llk::load(
-    new \Hoa\File\Read('Grammar.pp')
+        new \Hoa\File\Read('Grammar.pp')
 );
-$input    = 'b=2;print(b+1);';
-$ast      = $compiler->parse($input);
-$visitor  = new \Hoathis\Lua\Visitor\Interpreter();
-
-class A {
-    public $a;
+$input = implode(' ', array_slice($argv, 1));
+$ast = null;
+try {
+    $ast = $compiler->parse($input);
+} catch (\Hoa\Compiler\Exception $e) {
+    echo 'Impossible de parser : ', $e->getMessage();
 }
-$a = new A();
-$a->a = 1;
-$a = ['a' => 1];
-$b = 2;
-$env = $visitor->getRoot();
-$env->wrap('a', $a);
-$env->wrap('b', $b);
-//var_dump($a,$b);
-$visitor->visit($ast);
-//var_dump($a,$b);
+if ($ast) {
+    $visitor = new \Hoathis\Lua\Visitor\Interpreter();
+    try {
+        $visitor->visit($ast);
+    } catch (\Hoathis\Lua\Exception\Interpreter $e) {
+        echo 'Erreur à l\'exécution : ', $e->getMessage();
+    }
+}
