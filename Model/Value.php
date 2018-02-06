cab5631517491c575817c0c541a2e29509d73f41
Guislain Duthieuw <gduthieuw@houra.fr>
2018-02-06 14:45:13 +0100

    Nouvelle classe Value

--- Model/Value.php
+++ Model/Value.php
@@ -1,162 +1,37 @@
 <?php
-
-/**
- * Hoa
- *
- *
- * @license
- *
- * New BSD License
- *
- * Copyright © 2007-2013, Ivan Enderlin. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Hoa nor the names of its contributors may be
- *       used to endorse or promote products derived from this software without
- *       specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
-namespace {
-
-from('Hoathis')
+namespace Hoathis\Lua\Model;
 
 /**
- * \Hoathis\Lua\Exception\Model
- */
--> import('Lua.Exception.Model');
-
-}
-
-namespace Hoathis\Lua\Model {
-
-/**
- * Class \Hoathis\Lua\Model\Variable.
+ * Description of Value
  *
- * Variable.
- *
- * @author     Guislain Duthieuw <guislain.duthieuw@gmail.com>
- * @copyright  Copyright © 2007-2013 Ivan Enderlin.
- * @license    New BSD License
+ * @author houra
  */
+abstract class Value
+{
+    /**
+     *
+     * @var Hoathis\Lua\Model
+     */
+    protected $metatable;
 
-class Value {
-
-    protected $_value      = null;
-    protected $_referenceType = false;
-    protected $_container;
-    protected $setValueFunction;
-
-    const SCALAR = 0;
-    const REFERENCE = 1;
-
-
-    public function __construct ( $value, $referenceType = self::SCALAR) {
-
-        $this->_referenceType = $referenceType;
-        if ($this->_referenceType === self::REFERENCE) {
-            $this->_value       = new self($value);
-        } else {
-            $this->_value = $value;
-        }
-        $this->setValueFunction = array($this, 'setValueFunction');
-    }
-
-    public function setSetValueFunction($setValueFunction) {
-        $this->setValueFunction = $setValueFunction;
-    }
-
-    public function setValue($value) {
-        return call_user_func($this->setValueFunction, $value);
-    }
-
-    protected function setValueFunction ( $value ) {
-        if ($value instanceof self) {
-            $val = $value->getValue();
-        } else {
-            $val = $value;
-        }
-        if ($this->_referenceType === self::REFERENCE) {
-            $old          = $this->_value->getValue();
-            $this->_value->setValue($val);
-        } else {
-            $old          = $this->_value;
-            $this->_value = $val;
-        }
-        return $old;
-    }
-
-    public function getValue ( ) {
-        if ($this->_referenceType === self::REFERENCE) {
-            return $this->_value->getValue();
-        } else {
-            return $this->_value;
+    protected function initMetaTable() {
+        if(!$this->metatable instanceof Value\Table) {
+            $this->metatable = new Value\Table;
         }
     }
 
-    public function getReference() {
-        if ($this->_referenceType === self::REFERENCE) {
-            return $this->_value;
-        } else {
-            return $this;
-        }
+    public function getmetatable() {
+        $this->initMetaTable();
+        return $this->metatable;
     }
 
-    public function setReference($newReference) {
-        if ($this->_referenceType === self::REFERENCE) {
-            $old = $this->_value->getValue();
-        } else {
-            $old = $this->_value;
-        }
-        $this->_value = $newReference;
-        $this->_referenceType = self::REFERENCE;
-        return $old;
-    }
+    public function setmetatable(Value\Table $metatable) {
 
-    public function isReference() {
-        return $this->_referenceType === self::REFERENCE;
     }
-
-    public function getPHPValue() {
-        $val = $this->getValue();
-        if (true === is_array($val)) {
-            $result = array();
-            foreach ($val as $k => $v) {
-                if ($v instanceof self) {
-                    $result[$k] = $v->getPHPValue();
-                } else {
-                    $result[$k] = $v;
-                }
-            }
-            return $result;
-        }
-        return $this->getValue();
-    }
-
-    public function setContainer($container) {
-        $this->_container = $container;
-    }
-
-    public function getContainer() {
-        return $this->_container;
-    }
-}
-
 }
---
+++ Model/Value/Table.php
@@ -0,0 +1,13 @@
+<?php
+namespace Hoathis\Lua\Model\Value;
+/**
+ * Description of Table
+ *
+ * @author houra
+ */
+class Table
+{
+    const TYPE='table';
+
+    protected $values = [];
+}
