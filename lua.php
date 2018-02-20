1fb96c174ffbf8cd9bd2383a887fcd822049239d
Guislain Duthieuw <gduthieuw@houra.fr>
2018-02-20 15:08:44 +0100

    Fix lua.php

--- lua.php
+++ lua.php
@@ -5,19 +5,55 @@ if ($argc < 2) {
 }
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
-
+/**/
 $compiler = \Hoa\Compiler\Llk::load(
     new \Hoa\File\Read('Grammar.pp')
 );
 $visitor  = new \Hoathis\Lua\Visitor\Interpreter();
+$dump = new \Hoa\Compiler\Visitor\Dump();
 
 $input    = $argv[1];
 $start = microtime(true);
 $ast      = $compiler->parse($input);
+echo 'OK';
+echo $dump->visit($ast);
 (new \Hoathis\Lua\Visitor\LL2LR)->visit($ast);
+echo $dump->visit($ast);
 echo 'Parsed in ', round(1000 * (microtime(true) - $start)) . "ms\n";
 
 
 $start = microtime(true);
 $visitor->visit($ast);
 echo 'Executed in ', round(1000 * (microtime(true) - $start)) . "ms\n";
+/**/
+/*
+use Hoa\Compiler\Llk\Lexer as HoaLexer;
+use Hoa\Compiler\Llk\Llk;
+use Railt\Compiler\Grammar\Reader;
+use Railt\Compiler\Lexer as RailtLexer;
+use Railt\Io\File;
+require __DIR__ . '/vendor/autoload.php';
+$grammar = File::fromPathname('Grammar.pp');
+$sources = $argv[1];
+
+echo $grammar->getContents();
+
+$compiler = \Railt\Compiler\Parser::fromGrammar($grammar);
+exit();
+$visitor  = new \Hoathis\Lua\Visitor\Interpreter();
+$dump = new \Hoa\Compiler\Visitor\Dump();
+
+$input    = $argv[1];
+$start = microtime(true);
+$ast      = $compiler->parse($input);
+echo $dump->visit($ast);
+(new \Hoathis\Lua\Visitor\LL2LR)->visit($ast);
+//echo $dump->visit($ast);
+echo 'Parsed in ', round(1000 * (microtime(true) - $start)) . "ms\n";
+
+
+$start = microtime(true);
+$visitor->visit($ast);
+echo 'Executed in ', round(1000 * (microtime(true) - $start)) . "ms\n";
+
+ */
